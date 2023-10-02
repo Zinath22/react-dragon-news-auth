b7791aaadb188771fdca9718c0fb2b2ea0b951a9
Zinath22 <Zinath22@users.noreply.github.com>
2023-10-02 22:43:50 +0600

    environment and route setup

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgvTYfq3Q5BCWGlWwvLumj2lxvYbxBnlc",
+  authDomain: "react-dragon-auth-92f55.firebaseapp.com",
+  projectId: "react-dragon-auth-92f55",
+  storageBucket: "react-dragon-auth-92f55.appspot.com",
+  messagingSenderId: "837232468683",
+  appId: "1:837232468683:web:9fcb752dae7765b0c5f14e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
