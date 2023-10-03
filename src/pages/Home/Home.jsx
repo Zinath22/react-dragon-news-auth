ebbbf0631aa9228cfca474ff8b19a015340abdfe
Zinath22 <Zinath22@users.noreply.github.com>
2023-10-03 12:24:10 +0600

    rightside nav added'

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,11 +4,13 @@ import Header from "../Shared/Header/Header";
 import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
 import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+import BreakingNews from "./BreakingNews";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             {/* <h2 className="text-2xl font-poppins font-bold ">This is home page</h2> */}
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,48 @@
-
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
+import qzone1 from '../../../assets/qZone1.png';
+import qzone2 from '../../../assets/qZone2.png';
+import qzone3 from '../../../assets/qZone3.png';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right Side</h2>
+           <div className='p-4 space-y-3 mb-6'>
+            <h2 className="text-3xl">Login With</h2>
+         <button className="btn btn-outline w-full">
+               <FaGoogle></FaGoogle>
+           Google
+</button>
+         <button className="btn btn-outline w-full">
+               <FaGithub></FaGithub>
+           Github
+</button>
+     
+           </div>
+
+           <div className='p-4  mb-6'>
+            <h2 className="text-3xl mb-4">Find Us on</h2>
+               <a className='p-4 flex font-lg items-center border rounded-t-lg' href="">
+                <FaFacebook className='mr-3'></FaFacebook>
+                <span>FaceBook</span>
+                </a> 
+               <a className='p-4 flex font-lg items-center border-x' href="">
+                <FaTwitter className='mr-2'></FaTwitter>
+                <span>Twitter</span>
+                </a> 
+               <a className='p-4 flex font-lg items-center border rounded-b-lg' href="">
+                <FaInstagram className='mr-2'></FaInstagram>
+                <span>Instagram</span>
+                </a> 
+     
+           </div>
+           {/* QZone */}
+           <div className='p-4 space-y-3 mb-6'>
+            <h2 className="text-3xl">Login With</h2>
+             <img src={qzone1} alt="" />
+             <img src={qzone2} alt="" />
+             <img src={qzone3} alt="" />
+     
+           </div>
         </div>
     );
 };
